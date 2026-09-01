3d133cdc0a1391a43416ea93f7e044082bc10c76
taze292 <iamthekingtaze@proton.me>
2026-09-01 06:56:44 -0500

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -49,7 +49,7 @@ do
     ThemeManager.BuiltInThemes = {
         ["Default"] = {
             1,
-            { FontColor = "ffffff", MainColor = "191919", AccentColor = "f5fffa", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
+            { FontColor = "ffffff", MainColor = "191919", AccentColor = "#c2c2c2", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
         },
         ["BBot"] = {
             2,
